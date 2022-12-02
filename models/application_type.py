c19aaaf18840545d57aff650819184014577800b
Ahmad Barazi <ahmad.barazi@edm.com.lb>
2022-12-02 13:23:18 +0200

    adding status change date

--- models/application_type.py
+++ models/application_type.py
@@ -11,6 +11,12 @@ class ApplicationType(models.Model):
     status = fields.Selection(
         [('draft', 'Draft'), ('approved', 'Approved'), ('submitted', 'Submitted'), ('rejected', 'Rejected')],
         string="Status", default='draft')
+    status_change_date = fields.Datetime(string="Status Change Date")
     remarks = fields.Text(string="Remarks")
 
     reverse_id = fields.Many2one('ngo.beneficiary.application')
+
+    def write(self, values):
+        if values['status']:
+            values['status_change_date'] = datetime.utcnow()
+        return super(ApplicationType, self).write(values)
